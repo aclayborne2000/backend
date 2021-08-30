45f74c7681dfdbca364708296f5c11d326809414
aclayborne2000 <a_clayborne2000@yahoo.com>
2021-08-30 15:23:26 -0400

    Initial commit

---
+++ ShoppingListApi/Controllers/ShoppingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingListApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingListApi.Controllers
+{
+    public class ShoppingController : ControllerBase
+    {
+        private readonly ShoppingDataContext _context;
+
+        public ShoppingController(ShoppingDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("shopping")]
+        public ActionResult GetShoppingList()
+        {
+            var items = _context.ShoppingItems.ToList();
+            return Ok(items);
+        }
+    }
+}
---
+++ ShoppingListApi/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingListApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        // GET /status
+        [HttpGet("status")]
+        public ActionResult GetTheStatus()
+        {
+            // run some REAL code that checks out the status
+            var response = new StatusResponse
+            {
+                Message = "The Server is operating normally",
+                WhenChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+    public record StatusResponse
+    {
+        public string Message { get; init; }
+        public DateTime WhenChecked { get; init; }
+    }
+}
